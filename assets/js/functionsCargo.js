455f0b38ef37616d585c350b61cc855a64200569
vitorbarbieri <vbarbierisilva@gmail.com>
2023-09-26 16:13:49 -0300

    Ajustar configurações DataTable

--- assets/js/functionsCargo.js
+++ assets/js/functionsCargo.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     tableCargos = $('#tabelaCargo').dataTable({
         "aProcessing": true,
         "aServerSide": true,
+        "scrollY": true,
+        "scrollX": false, // Desativar scrool horizontal
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
         },
@@ -12,16 +14,19 @@ document.addEventListener('DOMContentLoaded', function () {
             "dataSrc": ""
         },
         "columns": [
-            { "data": "id" },
-            { "data": "nome" },
-            { "data": "descricao" },
-            { "data": "status" },
-            { "data": "opcao" }
+            { "data": "id", "width": "0%" },
+            { "data": "nome", "width": "15%" },
+            { "data": "descricao", "width": "60%" },
+            { "data": "status", "width": "10%" },
+            { "data": "opcao", "width": "15%" }
+        ],
+        "columnDefs": [
+            { "visible": false, "targets": 0 } // Desativar coluna de índice 0 (id)
         ],
         "resonsieve": "true",
         "bDestroy": true,
         "iDisplayLength": 10,
-        "order": [[0, "asc"]]
+        "order": [[1, "asc"]]
     });
 });
 
